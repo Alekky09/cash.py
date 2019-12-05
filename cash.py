09aa1636a9cb8756602cadd341f8e06b5a23f9a1
Alekky09 <Alekky09@users.noreply.github.com>
2019-12-05 00:48:16 +0000

    automated commit by check50 [check50=True]

--- cash.py
+++ cash.py
@@ -3,12 +3,20 @@ from cs50 import get_float
 while True:
 
     ch = get_float("Change owed: ")
-    if(ch > 0):
+    if ch > 0:
+
         cents = ch * 100
+
+
+
+
         q = cents // 25
-        d = (ch % 25) // 10
-        n = ((ch % 25) % 10) // 5
-        p = (((ch % 25) % 10) % 5) // 1
+        d = (cents % 25) // 10
+        n = ((cents % 25) % 10) // 5
+        p = (((cents % 25) % 10) % 5) // 1
+
         coins = q + d + n + p
+
         print(f"{coins:.0f}")
+
         break
